aa7edc2b0656c9161ac27acd8423060cf4cbdaeb
hoangmh27 <hoangmhhe173327@fpt.edu.vn>
2025-06-23 16:46:48 +0700

    feat(service, dto): implement ProductionOrder services and create P.O DTO

---
+++ SEP490/Modules/ProductionOrders/Services/IProductionOrdersService.cs
@@ -0,0 +1,9 @@
+﻿using SEP490.Modules.ProductionOrders.DTO;
+
+namespace SEP490.Modules.ProductionOrders.Services
+{
+    public interface IProductionOrdersService
+    {
+        public List<ProductionOrderListDto> GetAll();
+    }
+}
---
+++ SEP490/Modules/ProductionOrders/Services/ProductionOrdersService.cs
@@ -0,0 +1,36 @@
+﻿using SEP490.Common.Services;
+using SEP490.DB;
+using SEP490.DB.Models;
+using SEP490.Modules.ProductionOrders.Services;
+using SEP490.Modules.ProductionOrders.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace SEP490.Modules.ProductionOrders.Services
+{
+    public class ProductionOrdersService : BaseService, IProductionOrdersService
+    {
+        private readonly SEP490DbContext _context;
+        public ProductionOrdersService(SEP490DbContext context)
+        {
+            _context = context;
+        }
+        public List<ProductionOrderListDto> GetAll()
+        {
+            return _context.ProductionOrders
+                .Include(po => po.ProductionPlan)
+                    .ThenInclude(pp => pp.Customer)
+                .Include(po => po.ProductionPlan)
+                    .ThenInclude(pp => pp.PurchaseOrder)
+                .Select(po => new ProductionOrderListDto
+                {
+                    ProductionOrderCode = po.ProductionOrderCode,
+                    OrderCode = po.ProductionPlan.PurchaseOrder.OrderCode,
+                    CustomerName = po.ProductionPlan.Customer.CustomerName,
+                    TotalAmount = _context.ProductionOutputs
+                        .Where(poOut => poOut.ProductionOrderId == po.Id)
+                        .Sum(poOut => (int?)poOut.Amount ?? 0)
+                })
+                .ToList();
+        }
+    }
+}
