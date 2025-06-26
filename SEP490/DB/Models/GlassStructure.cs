465ae46edb3a29790dc73a208e78a55bf96fbb6e
kietnvt27 <kietnvthe179021@fpt.edu.vn>
2025-06-26 10:03:14 +0700

    update product code

--- SEP490/DB/Models/GlassStructure.cs
+++ SEP490/DB/Models/GlassStructure.cs
@@ -3,6 +3,7 @@ public class GlassStructure
 {
     public int Id { get; set; }
     public string? ProductName { get; set; }
+    public string? ProductCode { get; set; }
     public string? Category { get; set; }
     public string? EdgeType { get; set; }
     public string? AdhesiveType { get; set; }
