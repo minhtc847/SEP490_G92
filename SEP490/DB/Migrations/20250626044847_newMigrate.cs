faa91df1cc07668872ccd2e91de70ffe68272515
kietnvt27 <kietnvthe179021@fpt.edu.vn>
2025-06-26 12:25:17 +0700

    migrate

---
+++ SEP490/DB/Migrations/20250626044847_newMigrate.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SEP490.DB.Migrations
+{
+    public partial class newMigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_products_glass_structures_glass_structure_id",
+                table: "products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "glass_structure_id",
+                table: "products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "product_code",
+                table: "glass_structures",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_products_glass_structures_glass_structure_id",
+                table: "products",
+                column: "glass_structure_id",
+                principalTable: "glass_structures",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_products_glass_structures_glass_structure_id",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "product_code",
+                table: "glass_structures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "glass_structure_id",
+                table: "products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_products_glass_structures_glass_structure_id",
+                table: "products",
+                column: "glass_structure_id",
+                principalTable: "glass_structures",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
