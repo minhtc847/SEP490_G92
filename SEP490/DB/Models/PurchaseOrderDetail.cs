d1f55d67ea2fb4ce4aa9910a834b82988c535a35
kietnvt27 <kietnvthe179021@fpt.edu.vn>
2025-06-28 15:10:10 +0700

    fix database

---
+++ SEP490/DB/Models/PurchaseOrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SEP490.DB.Models
+{
+    public class PurchaseOrderDetail
+    {
+        public int Id { get; set; }
+        public int PurchaseOrderId { get; set; }
+        public string ProductName { get; set; }
+        public string Unit { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; } // Optional if calculated in app
+        public PurchaseOrder PurchaseOrder { get; set; }
+
+    }
+}
