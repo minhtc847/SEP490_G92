dab07677c296261eb8b7b8a51b9b7c840587f116
kietnvt27 <kietnvthe179021@fpt.edu.vn>
2025-07-07 21:46:57 +0700

    commit

--- fe/app/(defaults)/mockup/manager/ware-house-slips/[id]/page.tsx
+++ fe/app/(defaults)/mockup/manager/ware-house-slips/[id]/page.tsx
@@ -1,5 +1,5 @@
 import PhieuXuatKeoButylManager from '@/components/VNG/manager/phieu-xuat-keo-buytl-manager';
-// import WareHouseSlipDetailManager from '@/components/VNG/manager/ware-house-slip-detail-manager';
+import WareHouseSlipDetailManager from '@/components/VNG/manager/ware-house-slip-detail-manager';
 
 interface WareHouseSlipDetailPageProps {
     params: {
