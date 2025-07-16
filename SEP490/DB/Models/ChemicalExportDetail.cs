3d46addd2d58a9698e479a9831475d2404e0c82b
kietnvt27 <kietnvthe179021@fpt.edu.vn>
2025-07-17 03:40:50 +0700

    commit

--- SEP490/DB/Models/ChemicalExportDetail.cs
+++ SEP490/DB/Models/ChemicalExportDetail.cs
@@ -7,7 +7,6 @@
         public decimal Quantity { get; set; }
         public string? UOM { get; set; }
         public string? Note { get; set; } = string.Empty;
-
         public int? ProductionOrderId { get; set; }
         public ProductionOrder ProductionOrder { get; set; } = null!;
         public Product Product { get; set; } = null!;
