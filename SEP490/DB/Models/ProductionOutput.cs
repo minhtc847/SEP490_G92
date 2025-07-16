564442fb258d942fa799708f8756c528ca212647
kietnvt27 <kietnvthe179021@fpt.edu.vn>
2025-07-16 21:29:08 +0700

    commit

--- SEP490/DB/Models/ProductionOutput.cs
+++ SEP490/DB/Models/ProductionOutput.cs
@@ -7,7 +7,7 @@
         public string? ProductName { get; set; }
         public string? UOM { get; set; }
         public decimal? Amount { get; set; }
-        public int? Done { get; set; }
+        public int? Done { get; set; } = 0;
         public int? Broken { get; set; }
         public string? BrokenDescription { get; set; } 
         public string? Status { get; set; } // Trạng thái của sản phẩm: "Đã hoàn thành", "Đang sản xuất", "Bị hỏng", v.v.
