13361f9b9b05e298ff2930bdde7944281408288f
minhtc47 <minhtche170847@fpt.edu.vn>
2025-08-02 04:19:10 +0700

    feat: add MisaProductService

---
+++ SEP490/Selenium/Product/DTO/InputSingleProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace SEP490.Selenium.Product.DTO
+{
+    public class InputSingleProduct
+    {
+        public string Name { get; set; }
+        public string Type { get; set; } // e.g., "Hàng hóa". "Nguyên vật liệu", "Thành phẩm" Phai dung 1 trong 3
+        public string Unit { get; set; } // e.g., "m3", "kg", "cái", etc. Phai co san trong misa
+    }
+}
